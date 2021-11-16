78e8f3676a810b32f02edc65110e3f61377ead5b
taetae <peacepiece7@naver.com>
2021-11-16 09:12:27 +0900

    eslint 수정

--- front/sagas/no-immer-post.js
+++ front/sagas/no-immer-post.js
@@ -1,33 +1,33 @@
 // immer가 있고 없고 차이를 위해 남겨둔 파일
 
+import { all, delay, fork, put, takeLatest } from "redux-saga/effects";
+// import { nanoid } from "nanoid";
+// import axios from "axios";
 
-import { all, delay, fork, put, takeLatest } from 'redux-saga/effects'
-import { nanoid } from 'nanoid'
-import axios from 'axios'
-
-function addPostAPI(data) {
-  return axios.post('/api/posts', data)
-}
+// function addPostAPI(data) {
+//   return axios.post("/api/posts", data);
+// }
 
 function* addPost(action) {
   try {
     // const result = yield call(addPostAPI, action.data)
-    yield delay(1000)
+    yield delay(1000);
     yield put({
-      type: 'ADD_POST_SUCCESS',
-    })
+      type: "ADD_POST_SUCCESS",
+      data: action.data,
+    });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
+      type: "ADD_POST_FAILURE",
       data: err.response.data,
-    })
+    });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost)
+  yield takeLatest("ADD_POST_REQUEST", addPost);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)])
+  yield all([fork(watchAddPost)]);
 }
