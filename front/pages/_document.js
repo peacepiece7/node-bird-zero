add400d654c0c5644429242eb7643d7f248d172e
taetae <peacepiece7@naver.com>
2022-03-06 14:40:55 +0900

    add polyfill symbol

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head />
         <body>
-          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Cdom4%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020%2Ces5%2Ces6%2Ces7" />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Cdom4%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020%2Ces5%2Ces6%2Ces7%2CObject.getOwnPropertySymbols%2CSymbol%2CSymbol.asyncIterator%2CSymbol.for%2CSymbol.hasInstance%2CSymbol.isConcatSpreadable%2CSymbol.iterator%2CSymbol.keyFor%2CSymbol.match%2CSymbol.matchAll%2CSymbol.prototype.description%2CSymbol.replace%2CSymbol.search%2CSymbol.split%2CSymbol.species%2CSymbol.toPrimitive%2CSymbol.unscopables%2CSymbol.toStringTag%2Ces2021%2Ces2022" />
           <Main />
           <NextScript />
         </body>
